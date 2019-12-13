04f91f1b26f11e754b7afaf6e3578ef862c4c3be
danilozp <danilo.lopes@zephyrplace.com>
2019-12-13 15:21:05 -0300

    ex-01

---
+++ 01/index.js
@@ -0,0 +1,8 @@
+// – Calcule a média de diversas notas digitadas pelo usuário
+
+// var nota1 = prompt('Insira a sua primeira nota:')
+// var nota2 = prompt('Insira a sua segunda nota:')
+// var nota3 = prompt('Insira a sua terceira nota:')
+// var nota4 = prompt('Insira a sua quarta nota:')
+
+
